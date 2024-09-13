b3acd0ae04ff0d7abeb99fbbe33506c01cb76f84
Ario Nugroho <ario.n.suprapto@gmail.com>
2024-09-13 12:05:56 +0700

    init

---
+++ resources/views/template.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+  <title>Laravel 8 CRUD</title>
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
+</head>
+
+<body>
+
+  <div class="container">
+    @yield('content')
+  </div>
+
+</body>
+
+</html>
